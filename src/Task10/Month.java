56910a345987ea1f5ef6d39b5c8a231bb39a443c
Nikolay Stepanets <nik.stepanets@gmail.com>
2021-04-15 21:38:25 +0300

    added Task 10, ex. 1

---
+++ src/Task10/Month.java
@@ -0,0 +1,13 @@
+package Task10;
+
+public class Month {
+    String name;
+    int days;
+    int workDays;
+
+    public Month(String name, int days, int workDays) {
+        this.name = name;
+        this.days = days;
+        this.workDays = workDays;
+    }
+}
